4269d6f70f8813c1f87e8996e4dd30259b685395
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-04 17:42:56 -0300

    🚧 improve layout of the home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <div>
+      <Navbar />
+    </div>
+    <section class="main">
+      <CreateToDo @returnTodo="teste($event)" />
+    </section>
+    <main class="main">
+      <ToDo :item="todos" />
+    </main>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Provide, Vue } from 'vue-property-decorator';
+import Navbar from '../components/navbar/Navbar.vue';
+import ToDo from '../components/todo/ToDo.vue';
+import CreateToDo from '../components/todo/CreateToDo.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Navbar,
+    CreateToDo,
+    ToDo,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  @Provide() todos: { id: number; name: string; status: boolean } = { id: 0, name: '', status: false };
+
+  teste(item: { id: number; name: string; status: boolean }) {
+    this.todos = item;
+  }
+}
 </script>
