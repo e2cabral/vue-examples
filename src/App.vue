16abafe177315d67bf6844cbcc6616f764afc99e
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-04 17:42:34 -0300

    🚧 improve layout of the app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+@import url(./assets/css/global.css);
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100vh;
+  background: indigo fixed;
+  overflow-x: hidden;
 }
 
 #nav {
