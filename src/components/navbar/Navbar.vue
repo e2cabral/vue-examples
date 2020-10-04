eeb10724fc1fd777c76c2fd84a8dab1ac4721b37
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-04 17:46:13 -0300

    ✨ add a navbar

---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="navbar">
+    <section class="navbar__link">
+      <router-link to="/">Home</router-link>
+    </section>
+  </section>
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Navbar extends Vue {
+}
+</script>
+
+<style scoped>
+
+</style>
